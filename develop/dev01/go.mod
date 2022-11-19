c1b87fd55502bd153000bf59586713e93bc6419b
Marat <razorlevel@gmail.com>
2022-11-19 13:42:31 +0300

    solved dev01

---
+++ develop/dev01/go.mod
@@ -0,0 +1,11 @@
+module task1
+
+go 1.19
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+)
--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGetCurrentTime(t *testing.T) {
+	gotTime, err := getCurrentTime()
+
+	if err != nil {
+		t.Errorf("Got error from ntp lib: %s", err.Error())
+	}
+
+	if time.Now().Unix()-gotTime.Unix() > 2 {
+		t.Errorf("The time from ntp has a discrepancy greater than 2 seconds")
+	}
+
+}
