b2bc8a22eebdbdebb28ba67597ba4e97ad29a067
Marat <razorlevel@gmail.com>
2022-11-19 15:42:24 +0300

    solved dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -19,5 +28,55 @@ package main
 */
 
 func main() {
+	a, err := unpackString("a4bc2d5e")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(a)
+	os.Exit(0)
+}
 
+func unpackString(s string) (string, error) {
+
+	var newStr strings.Builder
+	runes := []rune(s)
+
+	for i := 0; i < len(runes); {
+		var numStr strings.Builder
+
+		if !unicode.IsDigit(runes[i]) {
+			// ! это буква !
+			j := i + 1
+			for ; j < len(runes); j++ {
+
+				if unicode.IsDigit(runes[j]) {
+					// ! это число !
+					numStr.WriteRune(runes[j])
+				} else {
+					break
+				}
+
+			}
+
+			count, err := strconv.Atoi(numStr.String())
+
+			if err != nil {
+				newStr.WriteRune(runes[i])
+			} else {
+				for u := 0; u < count; u++ {
+					newStr.WriteRune(runes[i])
+
+				}
+			}
+			i = j
+
+		} else {
+			return newStr.String(), errors.New("Incorrect str")
+			// ! ошибка не корректная строка
+		}
+
+	}
+
+	return newStr.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,45 @@
 package main
+
+import "testing"
+
+func TestUnpackString(t *testing.T) {
+	validData := map[string]string{
+		"a4bc2d5e": "aaaabccddddde",
+		"abcd":     "abcd",
+		"":         "",
+	}
+
+	t.Run("valid_data", func(t *testing.T) {
+
+		for inpStr, expectedStr := range validData {
+			t.Run(inpStr, func(t *testing.T) {
+				t.Parallel()
+				gotStr, err := unpackString(inpStr)
+
+				if err != nil {
+					t.Errorf("Getted error from unpackString func: %s", err.Error())
+				}
+
+				if expectedStr != gotStr {
+					t.Errorf("Incorrect result: \nexpected:%s\ngot:%s", expectedStr, gotStr)
+				}
+
+			})
+		}
+
+	})
+
+	t.Run("incorrect_data", func(t *testing.T) {
+		gotStr, err := unpackString("45")
+
+		if err == nil {
+			t.Errorf("unknown error with incorrect string")
+		}
+
+		if gotStr != "" {
+			t.Errorf("Incorrect result: \nexpected:''\ngot:%s", gotStr)
+		}
+
+	})
+
+}
