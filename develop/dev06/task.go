e84dfb79d51d52a4071648db1dfa4be6715c44cc
Marat <razorlevel@gmail.com>
2022-11-28 18:11:33 +0300

    solved dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -14,5 +27,132 @@ package main
 */
 
 func main() {
+	var fields, delimiter string
+	var separated bool
+	flag.StringVar(&fields, "f", "", "выбор колонок. Поумолчанию выводятся все")
+	flag.StringVar(&delimiter, "d", "\t", "использовать другой разделитель. Поумолчанию TAB")
+	flag.BoolVar(&separated, "s", false, "только строки с разделителем")
+	flag.Parse()
 
+	fieldsInNumArr, err := parseFields(fields)
+	if err != nil {
+		errStr := fmt.Sprintf("error: %v.\n", err.Error())
+		_, _ = os.Stderr.WriteString(errStr)
+		os.Exit(1)
+	}
+
+	fmt.Println("input text:")
+	reader := bufio.NewReader(os.Stdin)
+
+	var strRows []string
+	for {
+		// read line from stdin using newline as separator
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// if line is empty, break the loop
+		if len(strings.TrimSpace(line)) == 0 {
+			break
+		}
+
+		//append the line to a slice
+		strRows = append(strRows, line)
+	}
+
+	cutUtil := NewCutUtil(strRows, fieldsInNumArr, delimiter, separated)
+	// var output bytes.Buffer
+	cutUtil.cut(os.Stdout)
+}
+
+func parseFields(fieldsStr string) ([]int, error) {
+	if fieldsStr == "" {
+		return nil, errors.New("вы должны задать список полей")
+	}
+
+	var fields []int
+	var set = make(map[int]struct{})
+	diapazones := strings.Split(fieldsStr, ",")
+	for _, zone := range diapazones {
+		c, err := strconv.Atoi(zone)
+		if err != nil {
+			dz := strings.Split(zone, "-")
+			errTxt := "неправильный диапазон поля"
+			// fmt.Println(zone, dz)
+			if len(dz) != 2 {
+				return []int{}, errors.New(errTxt)
+			}
+
+			num1, err1 := strconv.Atoi(dz[0])
+			num2, err2 := strconv.Atoi(dz[1])
+
+			if err1 != nil || err2 != nil {
+				return nil, errors.New(errTxt)
+			}
+
+			if num2 < num1 {
+				return nil, errors.New("неверный уменьшающийся диапазон")
+			}
+
+			for i := num1 - 1; i < num2; i++ {
+				if _, ok := set[i]; !ok {
+					fields = append(fields, i)
+					set[i] = struct{}{}
+				}
+			}
+
+		} else {
+			if _, ok := set[c-1]; !ok {
+				fields = append(fields, c-1)
+				set[c-1] = struct{}{}
+			}
+		}
+
+	}
+
+	sort.Ints(fields)
+
+	return fields, nil
+}
+
+type CutUtil struct {
+	rowSlice []string
+	opts
+}
+
+type opts struct {
+	fields    []int
+	delimiter string
+	separated bool
+}
+
+func NewCutUtil(rows []string, fields []int, delimiter string, separated bool) CutUtil {
+	return CutUtil{rows, opts{fields, delimiter, separated}}
+}
+
+func (c CutUtil) cut(w io.Writer) {
+	for _, v := range c.rowSlice {
+		result := []string{}
+		splitedStr := strings.Split(v, c.delimiter)
+
+		if len(splitedStr) <= 1 {
+			if !c.separated {
+				fmt.Fprintln(w, strings.Trim(v, "\n"))
+			}
+			continue
+		}
+
+		for _, v := range c.fields {
+			if v > len(splitedStr)-1 {
+				break
+			}
+
+			result = append(result, splitedStr[v])
+		}
+
+		// fmt.Println(strings.Join(result, c.delimiter))
+		fmt.Fprintln(w, strings.TrimSpace(strings.Join(result, c.delimiter)))
+
+	}
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,140 @@
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func compare(t *testing.T, exp []int, got []int) {
+	if len(exp) != len(got) {
+		t.Fatalf("Expected:%v\nGot:%v", exp, got)
+	}
+
+	for i, v := range got {
+		if v != exp[i] {
+
+			t.Fatalf("Expected:%d\nGot:%d", exp[i], v)
+		}
+	}
+}
+
+func TestParseFields(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		expected := []int{0}
+		got, err := parseFields("1")
+		if err != nil {
+			t.Error(err)
+		}
+
+		compare(t, expected, got)
+
+	})
+
+	t.Run("1,2", func(t *testing.T) {
+		expected := []int{0, 1}
+		got, err := parseFields("1,2")
+		if err != nil {
+			t.Error(err)
+		}
+
+		compare(t, expected, got)
+	})
+
+	t.Run("1-3", func(t *testing.T) {
+		expected := []int{0, 1, 2}
+		got, err := parseFields("1-3")
+		if err != nil {
+			t.Error(err)
+		}
+
+		compare(t, expected, got)
+	})
+
+	t.Run("1-2,4", func(t *testing.T) {
+		expected := []int{0, 1, 3}
+		got, err := parseFields("1-2,4")
+		if err != nil {
+			t.Error(err)
+		}
+
+		compare(t, expected, got)
+	})
+
+	t.Run("4,1", func(t *testing.T) {
+		expected := []int{0, 3}
+		got, err := parseFields("4,1")
+		if err != nil {
+			t.Error(err)
+		}
+
+		compare(t, expected, got)
+	})
+
+}
+
+func TestCutUtil(t *testing.T) {
+	strRows := []string{
+		"zfds 310 1 xvcxv gfdgfdg",
+		"cfdf 1 5 bx fds",
+		"dsad 11 231 zas xvdsf",
+		"aada 65 43 hgfh mfdg",
+		"aada 65 432 khj khghfg",
+		"aada 65 454 tret hjgfh",
+		"aada 65 2 nbvnvb ergfdg",
+		"aada 65 0 jhgjg jhgjhg",
+		"fdsfdsf;gdfgdfds;fsdfdsf",
+	}
+
+	t.Run("test1", func(t *testing.T) {
+		expected := "zfds 1\n" + "cfdf 5\n" + "dsad 231\n" + "aada 43\n" + "aada 432\n" + "aada 454\n" + "aada 2\n" + "aada 0\n" + "fdsfdsf;gdfgdfds;fsdfdsf\n"
+
+		fields, _ := parseFields("1,3")
+
+		cutUtil := NewCutUtil(strRows, fields, " ", false)
+		var gotStr bytes.Buffer
+		cutUtil.cut(&gotStr)
+		if gotStr.String() != expected {
+			t.Fatalf("Not equal error\nExpected:\n%s\nGot:\n%s", expected, gotStr.String())
+		}
+	})
+
+	t.Run("test2", func(t *testing.T) {
+		expected := "310\n" + "1\n" + "11\n" + "65\n" + "65\n" + "65\n" + "65\n" + "65\n" + "fdsfdsf;gdfgdfds;fsdfdsf\n"
+
+		fields, _ := parseFields("2")
+
+		cutUtil := NewCutUtil(strRows, fields, " ", false)
+		var gotStr bytes.Buffer
+		cutUtil.cut(&gotStr)
+		if gotStr.String() != expected {
+			t.Fatalf("Not equal error\nExpected:\n%s\nGot:\n%s", expected, gotStr.String())
+		}
+	})
+
+	t.Run("test3", func(t *testing.T) {
+		expected := "310 1 xvcxv\n" + "1 5 bx\n" + "11 231 zas\n" + "65 43 hgfh\n" + "65 432 khj\n" + "65 454 tret\n" + "65 2 nbvnvb\n" + "65 0 jhgjg\n" + "fdsfdsf;gdfgdfds;fsdfdsf\n"
+
+		fields, _ := parseFields("2-4")
+
+		cutUtil := NewCutUtil(strRows, fields, " ", false)
+		var gotStr bytes.Buffer
+		cutUtil.cut(&gotStr)
+		if gotStr.String() != expected {
+			t.Fatalf("Not equal error\nExpected:\n%s\nGot:\n%s", expected, gotStr.String())
+		}
+	})
+
+	t.Run("test4 with separated=true", func(t *testing.T) {
+		expected := "zfds 1\n" + "cfdf 5\n" + "dsad 231\n" + "aada 43\n" + "aada 432\n" + "aada 454\n" + "aada 2\n" + "aada 0\n"
+
+		fields, _ := parseFields("1,3")
+
+		cutUtil := NewCutUtil(strRows, fields, " ", true)
+		var gotStr bytes.Buffer
+		cutUtil.cut(&gotStr)
+		if gotStr.String() != expected {
+			t.Fatalf("Not equal error\nExpected:\n%s\nGot:\n%s", expected, gotStr.String())
+		}
+	})
+
+}
